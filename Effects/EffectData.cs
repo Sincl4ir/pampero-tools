7f309988f9574aa06f57748d25ab6ebbbcce2351
Leandro Lopez <llopez.unitydev@gmail.com>
2025-02-09 17:22:36 -0300

    Created EffectData.

---
+++ Effects/EffectData.cs
@@ -0,0 +1,15 @@
+﻿namespace Pampero.Tools.Effects
+{
+    public struct EffectData : IEffectData
+    {
+        public IEffectTarget EffectTarget { get; }
+        public IEffectSource EffectSource { get; }
+        
+        public EffectData(IEffectTarget effectTarget, IEffectSource effectSource)
+        {
+            EffectTarget = effectTarget;
+            EffectSource = effectSource;
+        }
+    }
+}   
+//EOF.
